0c49ec36b84a57293c21a31eb49573462493357b
FadhilJemAR <fadhilar91128@gmail.com>
2026-07-25 12:17:27 +0800

    Add PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-
+import { VitePWA } from 'vite-plugin-pwa'
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType:'autoUpdate',
+      includeAssets:['favicon.svg','icons.svg'],
+      manifest:{
+        name:'Kanban Board',
+        short_name:"KanbanB",
+        description:"Catat aktivitas atau tugas yang ingin kamu lakukan",
+        theme_color:"#ffffff",
+        
+      }
+    })
+  ],
 })
